50f91dc9e533bd5d9bf5b3d3b9060851fd2de818
sadoum27 <manousadou57@gmail.com>
2025-05-10 16:17:04 +0100

    Ajout du fichier current_datetime.sh

---
+++ current_datetime.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script qui affiche la date et l'heure actuelles
+
+echo "La date et l'heure actuelles sont :"
+# Affiche une étiquette informative
+
+date
+# Affiche la date et l'heure système actuelles
